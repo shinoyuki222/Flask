2b4c3b5d5dba8b3ea550aabbcb7b5a310c8360ce
shinoyuki222 <tangxq93@126.com>
2020-06-19 16:08:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -184,5 +184,5 @@ def jsonexample():
     return 'Todo...'
 
 if __name__ == '__main__':
-    # app.run(debug=True, port=5000, host='0.0.0.0') 
-    app.run(debug=True, port=5000, host='0.0.0.0') #run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host='0.0.0.0') 
+    # app.run(debug=True, port=5000) #run app in debug mode on port 5000
