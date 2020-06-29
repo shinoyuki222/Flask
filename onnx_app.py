a7fb5bf079efcc13985f5858233113d6b4faab27
shinoyuki222 <tangxq93@126.com>
2020-06-29 14:39:09 +0800

    Update onnx_app.py

--- onnx_app.py
+++ onnx_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request, render_template
 app = Flask(__name__)
 from load_onnx import NLU_module
 import argparse
@@ -14,17 +14,20 @@ def predict():
     input_sentence = request.args.get('text')
     results = Module.Inference(input_sentence)
 
-    return jsonify(results,encoding = 'gb2312')
+    return (results)
 
 
-@app.route('/form-example')
+@app.route('/form', methods=['GET'])
 def formexample():
-    return 'Todo...'
+    # read test_sentence
+    input_sentence = request.args.get('text')
+    results = Module.Inference(input_sentence)
+    return render_template('result.html', input_sentence=results["Input_sentence"], pred_lbls=results["Raw Labels"], pred_cls=results["Intent"], slot=results["Megred Mentions"])
 
 @app.route('/json-example')
 def jsonexample():
     return 'Todo...'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0') 
-    # app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    # app.run(debug=True, port=5000, host='0.0.0.0') 
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
